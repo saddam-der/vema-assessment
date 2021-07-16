4abd0f5655c327dc1d9fdf1928ce5f7f9af6dbb6
saddam <saddam.dermawan@gmail.com>
2021-07-16 14:10:00 +0700

    deploy

---
+++ precache-manifest.44a9f57a95c2c6d919b2d453c6ba2c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d0d97fcff0a7700d6c",
+    "url": "/vema-assessment/css/app.aca3e39c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vema-assessment/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e84619970903bb946f082e12970c44e6",
+    "url": "/vema-assessment/index.html"
+  },
+  {
+    "revision": "90d0d97fcff0a7700d6c",
+    "url": "/vema-assessment/js/app.01bbd56d.js"
+  },
+  {
+    "revision": "c0141cd72fb4812d68ee",
+    "url": "/vema-assessment/js/chunk-vendors.c06f9603.js"
+  },
+  {
+    "revision": "ff0f35d554039d685462c202b0f80339",
+    "url": "/vema-assessment/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vema-assessment/robots.txt"
+  }
+]);
